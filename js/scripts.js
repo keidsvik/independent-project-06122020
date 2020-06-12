e46702d85848e8525bce5d4bb124e8d3f126195c
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-12 14:34:02 -0700

    add in css styling and background image

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
-// BUSINESS LOGIC
 
-function robotSpeak(humanInput) {
-  for (let i = 0; i < humanInput.length(); i++);
-}
 
 // UI LOGIC
 $(document).ready(function() {
   $("form#input").submit(function(event) {
-   const humanInput = $("#userInput").val();
-   const output = humanInput;
+   const humanInput = $("#input").val();
+   //let humanInputArray = humanInput.toString().split('')
+   //indexOfHumanInputString = humanInputString.indexOf();
+   const output = robotSpeak(humanInput);
    console.log(output);
   // parse user input parseInt($("input#input").val());
 
@@ -16,3 +14,15 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
+
+function robotSpeak(number) {
+  let humanArray = number.toString().split('');
+    if (humanArray === 1) {
+    return "AGHHHH";
+  }
+}
+
+
+
+
+
